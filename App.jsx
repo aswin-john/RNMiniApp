f2960b4b929a1032ae018db6ef8c8e27d61a0850
Aswin John <aswinjohnofficial478@gmail.com>
2025-12-19 21:56:14 +0530

    first commit

---
+++ App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        This is App component
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: '600',
+  },
+});
+
+export default App;
